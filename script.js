93169e8f2440cbdbd8a406373f6165736c8f01e4
bszeidl <szeidl.balazs@hotmail.com>
2022-12-24 21:15:26 +0100

    Fizz buzz rectangles are ready.

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+
+    let size = 50;
+    positionX = 20;
+    positionY = 15;
+
+    for (i = 1; i <=15; i++) {
+
+        if ( i % 15 === 0) {
+            context.fillStyle = 'rgba(0,255,0,.5)';
+        } else if ( i % 5 === 0) {
+            context.fillStyle = 'rgba(255,255,0,.5)';
+        } else if ( i % 3 === 0) {
+            context.fillStyle = 'rgba(0,0,255,.5)';
+        } else {
+            context.fillStyle ='rgba(0,0,0,.5)';
+        }
+
+        context.fillRect(positionX,positionY,size, size);
+        
+        positionX += 20;
+        positionY +=15;
+        
+
+    }
+      
 
 }
 
